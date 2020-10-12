2c1488b800f03abb195cc293d5e0f55ec00e69b6
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2020-10-12 18:40:37 -0300

    Add Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,44 @@
 'use strict'
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var babel = require('gulp-babel')
-var uglify = require('gulp-uglify')
-var cssnano = require('gulp-cssnano')
-var htmlmin = require('gulp-htmlmin')
-var sourcemaps = require('gulp-sourcemaps')
 var autoprefixer = require('gulp-autoprefixer')
+var babel = require('gulp-babel')
+var browserSync = require('browser-sync')
+var cssnano = require('gulp-cssnano')
+var gulp = require('gulp')
+var htmlmin = require('gulp-htmlmin')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify')
 
+// consts
 var htmlDir = './src/*.html'
-var scssDir = './src/scss/**/*.scss'
 var jsDir = './src/js/**/*.js'
+var scssDir = './src/scss/**/*.scss'
 
+// minifiers
 function minifyHtml(cb) {
   gulp
     .src(htmlDir)
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream())
+
+  cb()
+}
+
+function minifyJs(cb) {
+  gulp
+    .src(jsDir)
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream())
 
   cb()
 }
@@ -35,34 +56,30 @@ function minifyCss(cb) {
     .pipe(cssnano())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream())
 
   cb()
 }
 
-function minifyJs(cb) {
-  gulp
-    .src(jsDir)
-    .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: ['@babel/env'],
-      }),
-    )
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./build/js'))
+// exports
+function build(cb) {
+  gulp.parallel(minifyHtml, minifyCss, minifyJs)
 
   cb()
 }
 
 function start(cb) {
-  gulp.watch(
-    [htmlDir, scssDir, jsDir],
-    gulp.parallel(minifyHtml, minifyCss, minifyJs),
-  )
+  browserSync.init({
+    server: './build',
+  })
+
+  gulp.watch(htmlDir, minifyHtml)
+  gulp.watch(scssDir, minifyCss)
+  gulp.watch(jsDir, minifyJs)
+  gulp.watch([htmlDir, scssDir, jsDir]).on('change', browserSync.reload)
 
   cb()
 }
 
-exports.build = gulp.parallel(minifyHtml, minifyCss, minifyJs)
+exports.build = build
 exports.start = start
